e31e2e2fccbc1b944193c3ce69b5ae2bd2dfe34a
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-14 13:28:47 -0500

    Configurado el tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{html,js}",
   ],
   theme: {
-    extend: {},
+    screen: {
+      'xl': '1440px',
+    },
+
+    extend: {
+      colors:{
+        'Very-Dark-Grayish-Blue': 'hsl(217, 19%, 35%)',
+        'Desaturated-Dark-Blue': 'hsl(214, 17%, 51%)',
+        'Grayish-Blue': 'hsl(212, 23%, 69%)',
+        'Light-Grayish-Blue': 'hsl(210, 46%, 95%)',
+      },
+
+      fontFamily: {
+        'Manrope': ['Manrope','sans-serif'],
+      },
+
+    },
   },
   plugins: [],
 }
