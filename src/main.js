6de85099abfd975f0a4fc94bace95429815a68e8
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-14 19:22:31 -0500

    Agregado el footer movil y animado UwU, ahora a por el desktop

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const footerMovil = document.querySelector('#footer_movil');
+//console.log(footerMovil);
+const iconShareBefore = document.querySelector('#icon_share-before');
+//console.log(iconShareBefore);
+const iconShareAfter = document.querySelector('#icon_share-after');
+//console.log(iconShareAfter);
+
+iconShareBefore.addEventListener('click', () => {
+    footerMovil.classList.toggle('left-[-85vw]');
+    footerMovil.classList.toggle('left-0');
+});
+
+iconShareAfter.addEventListener('click', () => {
+    footerMovil.classList.toggle('left-[-85vw]');
+    footerMovil.classList.toggle('left-0');
+});
